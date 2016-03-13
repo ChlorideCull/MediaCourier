66f4f83061f5bd505d3a224817b120a561a91358
Chloride Cull <chloride@devurandom.net>
2016-03-13 15:09:51 +0100

    actually make mediacourier.py do stuff

--- MC-StreamPlexer/mediacourier.py
+++ MC-StreamPlexer/mediacourier.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-import sys
+import os.path
+import argparse
 
-if (sys.argv[1] == "health"):
-    print("okay i guess") # I promise this will be more useful later
-    exit(0)
+def print_status(leftstring, isgood, rightstring="\u2588"):
+    rharray = ["\x1B[31m", rightstring, "\x1B[39m"]
+    if isgood:
+        rharray[0] = "\x1B[32m"
+    print("{:<30}{:>30}".format(leftstring, "".join(rharray)))
 
-exit(1)
+def main():
+    defined_actions=["health"]
+
+    argparser = argparse.ArgumentParser(description="Manage the MediaCourier Installation.")
+    argparser.add_argument('action', metavar='<action>', help="Action to perform, such as \"health\".", choices=defined_actions)
+    argv = argparser.parse_args()
+    
+    if (argv.action == "health"):
+        print_status("Is nginx running?", os.path.isfile("/var/run/mediacourier_nginx.pid"))
+        
