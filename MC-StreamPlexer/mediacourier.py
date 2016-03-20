db3da51ce9e58699bd3f075a5b347b75ac8d1217
Chloride Cull <chloride@devurandom.net>
2016-03-20 16:18:16 +0100

    oh i just forgot a return

--- MC-StreamPlexer/mediacourier.py
+++ MC-StreamPlexer/mediacourier.py
@@ -12,7 +12,8 @@ def print_status(leftstring, isgood, rightstring="\u2588"):
 def init_parser_for_action(actionsel, actiondesc):
     argparser = argparse.ArgumentParser(description="Manage the MediaCourier Installation.")
     argparser.add_argument('action', metavar=actionsel, action='store_const', const=actionsel, help=actiondesc)
-    
+    return argparser
+
 def parse_all_args():
     defined_actions=["health", "start-publish", "finalize-recording"]
     argparser = argparse.ArgumentParser(description="Manage the MediaCourier Installation.",
@@ -23,15 +24,13 @@ def parse_all_args():
     
     if firststage.action == "health":
         argparser = init_parser_for_action(firststage.action, "Check the health of the MediaCourier instance.")
-        return argparser.parse_args()
     elif firststage.action == "start-publish":
         argparser = init_parser_for_action(firststage.action, "Flag a streamer as streaming, updating the database properly.")
         argparser.add_argument('name', metavar='<streamer name>', help="Name of the streamer who will be flagged as streaming.")
-        return argparser.parse_args()
     elif firststage.action == "finalize-recording":
         argparser = init_parser_for_action(firststage.action, "Finalize recordings, converting them to a smaller format.")
         argparser.add_argument('path', metavar='<stream recordings>', help="Path to the recordings generated.")
-        return argparser.parse_args()
+    return argparser.parse_args()
     
 def main():
     argv = parse_all_args()
